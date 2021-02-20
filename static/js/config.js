ce381c69cce3e72baabf722e503814e0c7d2873b
Dan Conrad <70244714+eggsnbacon97@users.noreply.github.com>
2021-02-20 10:33:16 -0500

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,3 +1,3 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
-// sorry, no API key for you. ;)
+const API_KEY = "pk.eyJ1IjoiZGFuaWVsamNvbnJhZCIsImEiOiJja2pjem5jdms1OTZ6MzByd3kwbTVmeWs0In0.G1uCyaUOiy_zCcbFHbI0XA";
+
